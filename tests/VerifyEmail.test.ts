01288e9f0784834deb3bcec56b51a088b1df70f1
yassine-allali-vinci <yassine.allali@student.vinci.be>
2026-01-16 17:41:01 +0100

    refactor: variable verify put in the beginning before tests begins

--- tests/VerifyEmail.test.ts
+++ tests/VerifyEmail.test.ts
@@ -3,17 +3,15 @@ import { VerifyEmail } from "../VerifyEmail";
 describe('VerifyEmail', () => {
 
     describe('Verify valid email', () => {
-        it("should reject email with spaces", () => {
-           let verify = new VerifyEmail();
+        let verify = new VerifyEmail();
 
+        it("should reject email with spaces", () => {
            let actual = verify.verifyValidEmail("exam @email.com");
 
             expect(actual).toBe("Invalid email: contains spaces");
         });
 
         it("should reject email without @", () => {
-            let verify = new VerifyEmail;
-
             let actual = verify.verifyValidEmail("examemail.com");
 
             expect(actual).toBe("Invalid email: missing @");
